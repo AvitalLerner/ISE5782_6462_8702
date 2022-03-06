0005388af7482d73678b2dd2b8f5afbf7cc05a7d
vaisberger <yaelw123@gmail.com>
2022-03-06 11:34:51 +0200

    6/3/22 final assignment 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import static primitives.Util.*;
 
 /**
  * Test program for the 1st stage
- *
+ * by Avital Lerner 3242180972 avitalgo444@gmail.com
+ * and Yael Vaisberger 211526462 yaelw123@gmail.com
  * @author Dan Zilberstein
  */
 public final class Main {
