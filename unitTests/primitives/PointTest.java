3e5707cc74875c9792fbb8c68a6993dd1e59a631
vaisberger <yaelw123@gmail.com>
2022-03-13 13:34:18 +0200

    13/3/22

--- unitTests/primitives/PointTest.java
+++ unitTests/primitives/PointTest.java
@@ -10,18 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class PointTest {
     Point p1 = new Point(1, 2, 3);
+
+    /**
+     * Test method for{@link Point#subtract(Point)}
+     */
     @Test
     void testSubtract() {
         assertEquals(new Vector(1,1,1),new Point(2,3,4).subtract(p1),"ERROR: Point - Point does not work correctly");
       }
 
+    /**
+     * Test method for{@link Point#add(Vector)}
+     */
     @Test
     void testAdd() {
-        fail("not yet implemented");
+      assertEquals(new Point(2,3,4),p1.add(new Vector(1,1,1)),"Error: Point + Point does not work");
     }
 
+    /**
+     * Test method for{@link Point#distanceSquared(Point)}
+     */
     @Test
     void testDistanceSquared() {
+        Point point3 = new Point(0.5, 0, -100);
+        double distance = point3.distanceSquared(new Point(0, 0, -100));
+        assertEquals(0.25, distance, 0.0001, "Doesn't calculate correctly the squared distance ");
     }
 
     /**
@@ -31,6 +44,6 @@ class PointTest {
     void testDistance() {
         Point point3 = new Point(0.5, 0, -100);
         double distance = point3.distance(new Point(0, 0, -100));
-        assertEquals(0.5, distance, 0.0001, "wrong ");
+        assertEquals(0.5, distance, 0.0001, "Doesn't calculate correctly the distance");
     }
 }
--- unitTests/primitives/VectorTest.java
+++ unitTests/primitives/VectorTest.java
@@ -4,9 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
 
 class VectorTest {
+    Vector v1=new Vector(1,2,3);
+    Vector v2=new Vector(-2,-4,-6);
+    Vector v3=new Vector(0,3,-2);
 
+    /**
+     * Test method for{@link Vector#Vector(Double3)}
+     */
     @Test
     public void testConstructorZero(){
         assertThrows(
@@ -15,31 +22,74 @@ class VectorTest {
                 "ERROR: zero vector should have thrown an exception"  );
         }
 
+    /**
+     * Test method for{@link Vector#lengthSquared()}
+     */
     @Test
     public void testLengthSquared() {
+        assertEquals(v1.lengthSquared(),14,"Doesn't calculates the length squared correctly ");
     }
 
+    /**
+     * Test method for{@link Vector#length()}
+     */
     @Test
     public void testLength() {
+        assertEquals(new Vector(0,3,4).length(),5,"Doesn't calculates the length squared correctly ");
     }
 
+    /**
+     * Test method for{@link Vector#dotProduct(Vector)}
+     */
     @Test
     public void testDotProduct() {
+        assertEquals(v1.dotProduct(v3),0,"ERROR: dotProduct() for orthogonal vectors is not zero");
+        assertEquals(0,v1.dotProduct(v2) + 28,"ERROR: dotProduct() wrong value");
     }
 
+    /**
+     * Test method for{@link Vector#crossProduct(Vector)}
+     */
     @Test
     public void testCrossProduct() {
+        assertThrows(
+        IllegalArgumentException.class,
+                () -> {v1.crossProduct(v2);},
+                "ERROR: crossProduct() for parallel vectors does not throw an exception");
+        Vector vr = v1.crossProduct(v3);
+        assertEquals(v1.length() * v3.length() ,vr.length() ,0.00001,"ERROR: crossProduct() wrong result length");
+        assertEquals(0,vr.dotProduct(v1),"ERROR: crossProduct() result is not orthogonal to its operands");
+        assertEquals(0,vr.dotProduct(v3),"ERROR: crossProduct() result is not orthogonal to its operands");
+
     }
 
+    /**
+     * Test method for{@link Vector#scale(double)}
+     */
     @Test
     public void testScale() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {v1.scale(0);},
+                "ERROR: scale(0) not valid and does not throw an exception ");
     }
 
+    /**
+     * Test method for{@link Vector#add(Vector)}
+     */
     @Test
     public void testAdd() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> { v1.add(new Vector(-1,-2,-3));},
+                "ERROR: add() resulting by 0 not valid and does not throw an exception");
     }
 
+    /**
+     * Test method for{@link Vector#normalize()}
+     */
     @Test
     public void testNormalize() {
+
     }
 }
