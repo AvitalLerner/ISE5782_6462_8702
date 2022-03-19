ce7a4ccf310017f2d07a689137a1db4933423adf
vaisberger <yaelw123@gmail.com>
2022-03-19 22:51:07 +0200

    19/3/22 5

--- unitTests/geometries/GeometriesTest.java
+++ unitTests/geometries/GeometriesTest.java
@@ -1,6 +1,9 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,23 @@ class GeometriesTest {
 
     @Test
     void testFindIntersections() {
+        Geometries listOfObjects=new Geometries();
+        Sphere sphere = new Sphere( new Point(1, 0, 0),1d);
+        Triangle tr = new Triangle(new Point(0,0,0), new Point(0,1,0), new Point(0,0,1));
+        Polygon polygon=new Polygon(new Point(0,1,0),new Point(0,0,0),new Point(0,0,1),new Point(0,1,1));
+        Tube tube=new Tube(new Ray(new Point(0, 0, 1), new Vector(0, -1, 0)),1.0);
+        Plane plane=new Plane(new Point(0, 0, 1), new Vector(1, 1, 1));
+        // =============== Boundary Values Tests ==================
+
+        //TC11: there are no objects to intersect
+        assertNull(listOfObjects.findIntersections(new Ray(new Point(0.5, 0, 0)
+                ,new Vector(1, 0, 0))),"no object to intersect");
+
+        //TC12: there is no object that has intersection with ray
+        listOfObjects.add(sphere,plane);
+        assertNull(listOfObjects.findIntersections(new Ray(new Point(-1,-1,-1),
+                new Vector(-3,-3,-3))),"no intersection supposed to be");
+        //TC13: only one object intersects
+
     }
 }
