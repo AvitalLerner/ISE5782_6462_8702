71c8d7f87eb61ae9b7917e8696e24ec7a64cdc8b
AvitalLerner <93719159+AvitalLerner@users.noreply.github.com>
2022-03-21 18:48:50 +0200

    13

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -38,5 +38,35 @@ public class Camera {
         return this;
     }
 
-    public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {return null;}
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i)
+    {
+        double Rx = _width / nX;
+        double Ry = _height / nY;
+
+        double xJ = (j -(nX-1)/2)*Rx;
+        double yI = i -(nY-1)/2*Ry;
+
+        Point pIJ=_p0.add(_vTo.scale(_distance));
+
+        if (isZero(xJ) && isZero(yI)) {
+            return new Ray(_p0, pIJ.subtract(_p0));
+        }
+
+        if (isZero(xJ))
+            pIJ = pIJ.add(_vUp.scale(yI));
+
+        if (isZero(yI))
+            pIJ = pIJ.add(_vRight.scale(xJ));
+
+
+        if(!isZero(yI)&&!isZero(xJ))
+            pIJ = pIJ.add(_vUp.scale(yI)).add(_vRight.scale(xJ));
+
+
+        return new Ray(_p0, pIJ.subtract(_p0));
+
+        //  Ray result = pIJ + _vRight.scale(xJ)-yI*_height;
+
+
+    }
 }
