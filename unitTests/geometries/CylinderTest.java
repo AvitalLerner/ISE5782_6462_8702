db33ba9c73424e5bbba742b9d9cd55cdfb6e4cdd
AvitalLerner <93719159+AvitalLerner@users.noreply.github.com>
2022-03-21 18:07:03 +0200

    12

--- unitTests/geometries/CylinderTest.java
+++ unitTests/geometries/CylinderTest.java
@@ -25,24 +25,29 @@ class CylinderTest {
     @Test
     void testFindIntersections() {
       Cylinder cylinder1=new Cylinder(1,new Ray(new Point(1, 0, 0), new Vector(0, 1, 0)));
-        Cylinder cylinder2=new Cylinder(1,new Ray(new Point(1, 1, 1), new Vector(0, 0, 1)));
+      Cylinder cylinder2=new Cylinder(1,new Ray(new Point(1, 1, 1), new Vector(0, 0, 1)));
         // ============ Equivalence Partitions Tests ==============
         // TC01: Ray's line is outside the cylinder
-        assertNull(cylinder1.findIntersections(new Ray(new Point(1, 1, 2), new Vector(1, 1, 0))),"there aren't supposed to be intersections");
+        assertNull(cylinder1.findIntersections(new Ray(new Point(1, 1, 2), new Vector(1, 1, 0))),
+                "there aren't supposed to be intersections");
 
         //// TC02: Ray's crosses the cylinder
         List<Point> result =cylinder2.findIntersections(new Ray(new Point(0, 0, 0), new Vector(2, 1, 1)));
-        assertEquals(2, result.size(), "must be 2 intersections");
+        assertEquals(2, result.size(),
+                "must be 2 intersections");
         if (result.get(0).getY() > result.get(1).getY()) {
             result = List.of(result.get(1), result.get(0));
         }
-        assertEquals(List.of(new Point(0.4, 0.2, 0.2), new Point(2, 1, 1)), result, "Intersections points incorrect");
+        assertEquals(List.of(new Point(0.4, 0.2, 0.2), new Point(2, 1, 1)), result,
+                "Intersections points incorrect");
 
         // TC03: Ray's starts within cylinder and crosses the cylinder
         result = cylinder2.findIntersections( new Ray(new Point(1, 0.5, 0.5), new Vector(2, 1, 1)));
 
-        assertEquals(1, result.size(), "must be 1 intersections");
-        assertEquals(List.of(new Point(2, 1, 1)), result, "Intersection point calculated wrong");
+        assertEquals(1, result.size(),
+                "must be 1 intersections");
+        assertEquals(List.of(new Point(2, 1, 1)), result,
+                "Intersection point calculated wrong");
 
     }
 
