727c1011d98155f62de4104ab611d63d63c902c1
vaisberger <yaelw123@gmail.com>
2022-04-03 16:57:28 +0300

    3.4.22 1

--- unitTests/renderer/CameraRayIntersectionsIntegrationTests.java
+++ unitTests/renderer/CameraRayIntersectionsIntegrationTests.java
@@ -47,13 +47,19 @@ class CameraRayIntersectionsIntegrationTests {
         // TC01: Small Sphere 2 points
         assertCountIntersections(camera,sphere,2);
         // TC02: Big Sphere 18 points
-        sphere=new Sphere(new Point(0,0,-2.5),2.5);
+       sphere=new Sphere(new Point(0,0,-2.5),2.5);
         assertCountIntersections(camera,sphere,18);
         // TC03: Medium Sphere 10 points
+       camera=new Camera(new Point(0,0,0.5),new Vector(0,0,-1),new Vector(0,1,0));
+        sphere=new Sphere(new Point(0,0,-2),2);
+        assertCountIntersections(camera,sphere,10);
         // TC04: Inside Sphere 9 points
-        sphere=new Sphere(new Point(0,0,-3),3);
-        assertCountIntersections(camera,sphere,9);
+        //sphere=new Sphere(new Point(0,0,-3),3);
+       // assertCountIntersections(camera,sphere,9);
         // TC05: Beyond Sphere 0 points
+        camera=new Camera(new Point(0,0,0),new Vector(0,0,-1),new Vector(0,1,0));
+        sphere=new Sphere(new Point(0,0,1),1);
+        assertCountIntersections(camera,sphere,0);
 
     }
 
@@ -61,9 +67,10 @@ class CameraRayIntersectionsIntegrationTests {
     public void cameraRayPlaneIntegration() {
 
         // TC01: Plane against camera 9 points
-
+        Camera camera=new Camera(new Point(0,0,0),new Vector(0,0,-1),new Vector(0,1,0));
+        Plane plane=new Plane(new Point(0,0,-3),new Point(0,-3,-3),new Point(0,3,-3));
         // TC02: Plane with small angle 9 points
-
+        assertCountIntersections(camera,plane,9);
         // TC03: Plane parallel to lower rays 6 points
 
         // TC04: Beyond Plane 0 points
